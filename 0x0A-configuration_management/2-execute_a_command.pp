4bca5e7e230da7e2e5aa4fbdc9082ca3a1dfc26d
Parrot <parrot@localhost.localdomain>
2022-03-13 04:00:44 +0300

    kill me now

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
-# Using Puppet, create a manifest that kills a process named killmenow
+# Manifest kills a process named 'killmenow'
 exec { 'killmenow':
-  command => 'pkill -f killmenow'
-  path    => ['/usr/bin', '/usr/sbin', '/bin']
+  command  => 'pkill killmenow',
+  provider => 'shell',
 }
